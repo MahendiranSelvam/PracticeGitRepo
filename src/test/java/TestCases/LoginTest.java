4bec45e579ccda544748646c8856b32c211183e7
mahendiran Selvam <mahendiran.selvam@gmail.com>
2021-03-14 23:36:13 -0400

    My Test Commit

---
+++ src/test/java/TestCases/LoginTest.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void LoginMethod(){
+
+        System.out.println("Inside Login Method");
+
+    }
+
+}
---
+++ src/test/java/TestCases/MyAccountTest.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class MyAccountTest {
+
+    @Test
+    public void MyAccountMethod(){
+
+        System.out.println("Inside MyAccount Method");
+    }
+
+
+}
